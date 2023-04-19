1ac6951a2607f1b55bc7524d1502b3ea50db946b
rkisztian <radeczki.krisztian@gmail.com>
2023-04-19 21:43:44 +0200

    Kiegészítések

--- src/Pages/AboutusPage.tsx
+++ src/Pages/AboutusPage.tsx
@@ -2,7 +2,8 @@ import { Component } from "react";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import "bootstrap/dist/css/bootstrap.css"
-
+import { Container } from "react-bootstrap";
+import Logo from "../Images/sky_fitness_logo.png"
 
 interface State{
 
@@ -12,8 +13,26 @@ export default class AboutusPage extends Component<{}, State> {
 
 
     render(){
-        return <div>
-
-        </div>
+        return <Container>
+            <section className="Aboutus">
+                <h1>Rólunk :</h1>
+            <div className="conatiner">
+                <div className="row">
+                    <div className="col-5">
+                        <h2>A csapatról:</h2>
+                        <p>A Sky Fitness csapata segítséget nyujt a kezdőknek és a haladóknak is egyaránt
+                            Itt minden izomcsoportra a lehető legjobb gyakorlatokat szedtük össze, hogy egyszerűbb legyen megtalálni a gyakorlatokat
+                            De természetesen folyamatosan bővítve lesznek a gyakorlatok száma és a jövőben előre elkészített edzéstervek is elérhetők lesznek.
+                        </p>
+                        <h6>Az oldalt készítette: Radeczki Krisztián</h6>
+                    </div>
+                    <div className="col-7">
+                        <img src={Logo} ></img>
+                    </div>
+                </div>
+            </div>
+            </section>
+            
+        </Container>
     }
 }
