eb801d22a50ee08e463bb7d5f748da27ca05f457
rkisztian <radeczki.krisztian@gmail.com>
2023-04-20 22:04:26 +0200

    lényegtelen vűltoztatások

--- src/Pages/CalorieCalculator.tsx
+++ src/Pages/CalorieCalculator.tsx
@@ -4,15 +4,66 @@ import Header from "../Components/Header";
 import "bootstrap/dist/css/bootstrap.css"
 
 
-interface State{
+interface IState {
+    weight: number;
+    height: number;
+    age: number;
+    gender: string;
+    activityLevel: string;
+    dailyCalories: number;
+  }
+export default class Caloriecalculator extends Component<{}, IState> {
 
-}
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            weight: 0,
+            height: 0,
+            age: 0,
+            gender: '',
+            activityLevel: '',
+            dailyCalories: 0
+        };
+    }
 
-export default class Caloriecalculator extends Component<{}, State> {
+      /**
+       * handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value } as Pick<IState, keyof IState>);
+      };
+       */
+    
+    
 
+      calculateCalories = () => {
+        const { weight, height, age, gender, activityLevel } = this.state;
+        let dailyCalories = 0;
+    
+        if (gender === 'male') {
+          dailyCalories = 10 * weight + 6.25 * height - 5 * age + 5;
+        } else if (gender === 'female') {
+          dailyCalories = 10 * weight + 6.25 * height - 5 * age - 161;
+        }
+    
+        if (activityLevel === 'sedentary') {
+          dailyCalories *= 1.2;
+        } else if (activityLevel === 'lightlyActive') {
+          dailyCalories *= 1.375;
+        } else if (activityLevel === 'moderatelyActive') {
+          dailyCalories *= 1.55;
+        } else if (activityLevel === 'veryActive') {
+          dailyCalories *= 1.725;
+        } else if (activityLevel === 'extraActive') {
+          dailyCalories *= 1.9;
+        }
+    
+        this.setState({ dailyCalories });
+      };
 
     render(){
+        const { weight, height, age, gender, activityLevel, dailyCalories } = this.state;
         return <div>
+            
 
         </div>
     }
--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,16 +1,12 @@
 import { Component, ReactNode } from "react";
 import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Logo from "../Images/sky_fitness_logo.png"
 
 interface State{
     email: string,
     username: string,
 }
-
-interface Edzesterv{
-    id: number,
-    name: string,
-}
-
 
 
 export default class ProfilePage extends Component<{}, State>{
@@ -28,6 +24,14 @@ export default class ProfilePage extends Component<{}, State>{
     render(): ReactNode {
         
         return<Container>
+            <div className="Profile">
+                <img src={Logo} ></img>
+                <h1>Töltsd le a mobil applikációnkat Android eszközödre itt:</h1>
+                    <Link to="/*" className="link">Sky Fitness Mobil App</Link>
+                    <p>Kattints rá!</p>
+            </div>
+            
+            
 
         </Container>
     }
